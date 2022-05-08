483c7141e780fb08f60d70eb6627f2c9a7ad2c00
Henrique Marques <henrique_marques.dev@outlook.com>
2022-05-08 13:38:04 -0300

    feat: send email done

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "88cac65e826e59",
+    pass: "d029051140e335",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post("/feedbacks", async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedback <oi@feedback.com>",
+    to: "Henrique Marques <henrique_marques.dev@outlook.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div styles="font-family: sans-serif; font-size: 16px; color: #222222">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
   });
 
   return res.status(201).json({ data: feedback });
